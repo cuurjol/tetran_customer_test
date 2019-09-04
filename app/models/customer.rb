9b7fd943a6fefadf05eb15fdcd07ba38cd8f0611
Kirill Ilyin <cuurjol@gmail.com>
2019-09-04 15:14:19 +0300

    Added validates for Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < ApplicationRecord
+  VALID_PHONE_REGEX = (/\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/).freeze
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :phone, presence: true, length: { maximum: 15 }, format: { with: VALID_PHONE_REGEX }
 end
